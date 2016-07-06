704c2a115e14e2af2757539c42320980eed98a1d
hasarinda <x-hta@xinfinit.com>
2016-07-06 15:40:18 +0530

    correlation matrix added tooltip & axis

--- app/components/corelationMatrix/corelationMatrix.js
+++ app/components/corelationMatrix/corelationMatrix.js
@@ -1,18 +1,18 @@
 app.directive('corelationMatrix', [function(){
 
-    var controller = function($scope, QRDataService){
+    var controller = function($scope, $element, $log, QRDataService){
 
         var size = 10;
 
         this.onWidgetLoad = function(){
             QRDataService.getCorrelationMatrixData().then(function(data){
                 var processedData = processData(data);
-                drawChart(processedData, data.meta.symbols.length, size);
+                drawChart(processedData, data.meta.symbols, size);
             });
         };
 
         this.onDestroy = function(){
-            var svgContainer = d3.select("#corelationMatrix");
+            var svgContainer = d3.select($element.find("div#corelationMatrix").get(0));
             svgContainer.remove();
             $scope.$destroy();
         };
@@ -29,24 +29,25 @@ app.directive('corelationMatrix', [function(){
             angular.forEach(data.meta.symbols, function(mainValue, mainIndex){
                 angular.forEach(s1, function(secondaryValue, secondaryIndex){
                     if(mainValue !== secondaryValue && (mainIndex + secondaryIndex) < len){
-                        processedData.push(
-                            {
-                                x : mainIndex,
-                                y : secondaryIndex,
-                                b : true
-                            }
-                        );
                         angular.forEach(data.data[mainValue + secondaryValue], function(value, index){
                             processedData.push(
                                 {
-                                    t : mainValue + secondaryValue,
-                                    f : data.meta.fields[index],
                                     x : (mainIndex * 3) + (index % 3),
                                     y : (secondaryIndex * 3) + Math.floor(index / 3),
                                     v : value
                                 }
                             );
                         });
+                        processedData.push(
+                            {
+                                t : mainValue + secondaryValue,
+                                f : data.meta.fields,
+                                v : data.data[mainValue + secondaryValue],
+                                x : mainIndex,
+                                y : secondaryIndex,
+                                b : true
+                            }
+                        );
                     }
                 });
             });
@@ -57,48 +58,126 @@ app.directive('corelationMatrix', [function(){
         /**
          * draw the matrix chart
          * @param data
-         * @param elementCount
+         * @param symbols
          * @param elementSize
          */
-        var drawChart = function(data, elementCount, elementSize){
-            var svgContainer = d3.select("#corelationMatrix").append("svg").attr("width", elementCount * elementSize * 3).attr("height", elementCount * elementSize * 3);
+        var drawChart = function(data, symbols, elementSize){
 
+            var elementCount = symbols.length,
+                margin = {top: 40, right: 40, bottom: 40, left: 40},
+                width = elementCount * elementSize * 3 - margin.left - margin.right,
+                height = elementCount * elementSize * 3 - margin.top - margin.bottom;
+
+            var svgContainer = d3.select($element.find("div#corelationMatrix").get(0)).append("svg")
+                .attr("width", elementCount * elementSize * 3 + margin.left + margin.right)
+                .attr("height", elementCount * elementSize * 3 + margin.top + margin.bottom)
+                .style('background', '#1c1c1c')
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+            //Create the Scale for the x-Axis
+            var xAxisScale = d3.scaleLinear().domain([0, elementCount]).range([0, elementCount * elementSize * 3]);
+            //Create the x-Axis
+            var xAxis = d3.axisTop(xAxisScale).ticks(elementCount).tickFormat(function(d){
+                return symbols[d];
+            });
+            //Create an SVG group Element for the Axis elements and call the xAxis function
+            var xAxisGroup = svgContainer.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")").call(xAxis);
+            //align labels
+            xAxisGroup.selectAll('text').attr("transform", "translate(15, 0)").style('fill', 'white');
+
+            //Create the Scale for the y-Axis
+            var yAxisScale = d3.scaleLinear().domain([0, elementCount]).range([elementCount * elementSize * 3, 0]);
+            //Create the y-Axis
+            var yAxis = d3.axisLeft(yAxisScale).ticks(elementCount).tickFormat(function(d){
+                return symbols[d];
+            });
+            //Create an SVG group Element for the Axis elements and call the yAxis function
+            var yAxisGroup = svgContainer.append("g")
+                .attr("transform", "translate(" + margin.left + "," +  margin.top + ")")
+                .call(yAxis);
+            //align labels
+            yAxisGroup.selectAll('text').attr("transform", "translate(0,-15)").style('fill', 'white');
+
+            //// Define the div for the tooltip
+            var div = d3.select($element.find("div#corelationMatrix").get(0)).append('div').attr('class', 'tooltip').style("opacity", 0);
+
+            //create the data bozes
             var rectangles = svgContainer.selectAll("rect").data(data).enter().append("rect")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
                 .attr("x", function (d) {
-                    return d.b === true ? (3 * d.x * elementSize) : d.x * elementSize;
+                    return (d.b === true ? (3 * d.x * elementSize) : d.x * elementSize);
                 })
                 .attr("y", function (d) {
-                    return d.b === true ? (3 * d.y * elementSize) : d.y * elementSize;
+                    return (d.b === true ? (3 * d.y * elementSize) : d.y * elementSize);
                 })
                 .attr("width", function (d) {
                     return d.b === true ? (3 * elementSize) : elementSize;
-                }).attr("height", function (d) {
+                })
+                .attr("height", function (d) {
                     return d.b === true ? (3 * elementSize) : elementSize;
                 })
                 .style("stroke", function (d) {
                     return d.b === true ? 'black' : 'none'
                 })
                 .style("fill", function (d) {
-                    var returnColor;
-                    if (d.b === true) {
-                        returnColor = 'none';
-                    } else {
-                        if (d.v < 0) {
-                            var r = 255 - Math.ceil(Math.abs(d.v) / 100 * 255);
-                            returnColor = "rgb(255," + r + "," + r + ")";
-                        } else if (d.v === 0) {
-                            returnColor = "white";
-                        } else if (d.v > 0) {
-                            var g = 255 - Math.ceil(d.v / 100 * 255);
-                            returnColor = "rgb(" + g + ",255," + g + ")";
-                        }
-                    }
-                    return returnColor;
-                }).filter(function (d) {
-                    return d.b !== true;
-                }).append("svg:title").text(function (d) {
-                    return d.t + " : " + d.f + " => " + d.v;
+                    return d.b === true ? 'transparent' : getColor(d.v);
+                })
+                .filter(function (d) {
+                    return d.b === true;
+                })
+                .on("mouseover", function(d) {
+                    div.transition().duration(200).style("opacity", .9);
+                    var position = getRelativePosition(d3.event.pageX, d3.event.pageY), html = '<table><thead><tr><th colspan="3">' + d.t + '</th></tr></thead><tbody>';
+                    angular.forEach(d.f, function(value, index){
+                        html += '<tr><td><div style="background-color:' + getColor(d.v[index]) + ';width:15px;height:15px"></div></td><td>' + value + '</td><td>' + d.v[index] + '</td></tr>';
+                    });
+                    html += '</tbody></table>';
+                    div.html(html).style("left", (position.x) + "px").style("top", (position.y) + "px");
+                })
+                .on("mouseout", function(d) {
+                    div.transition().duration(500).style("opacity", 0);
                 });
+        };
+
+        /**
+         * get color according to value
+         * @param value
+         * @returns {*}
+         */
+        var getColor = function(value){
+            var returnColor;
+            if (value < 0) {
+                var r = 255 - Math.ceil(Math.abs(value) / 100 * 255);
+                returnColor = "rgb(255," + r + "," + r + ")";
+            } else if (value === 0) {
+                returnColor = "white";
+            } else {
+                var g = 255 - Math.ceil(value / 100 * 255);
+                returnColor = "rgb(" + g + ",255," + g + ")";
+            }
+            return returnColor;
+        };
+
+        /**
+         * get position according to layout
+         * @param x
+         * @param y
+         * @returns {*}
+         */
+        var getRelativePosition = function(x, y){
+            var rel = $element.offset();
+            var position = {x : x - rel.left, y : y - rel.top};
+            if($ && $.portal){
+                switch($.portal.config.layout.type){
+                    case 'flex':
+                        position = {x : x, y : y - $("div.AREA-NORTH").height()};
+                        break;
+                    default :
+                        break;
+                }
+            }
+            return position;
         };
 
     };
